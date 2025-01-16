38fa2a5db63f4ed56418ba9b9b7aaa9c8815f713
kenzieshane <111677159+kenzieshane@users.noreply.github.com>
2025-01-16 20:41:05 +0700

    Update totally_legit_scripts.js (#2)

---
+++ totally_legit_scripts.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4L53PM9WYD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4L53PM9WYD');
+</script>
